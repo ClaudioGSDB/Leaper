10f6da748daab6021986c7a9dbac61a950d55cfa
Claudio Sciotto <fisizion@gmail.com>
2023-09-26 01:45:55 -0400

    Improved Movement, Added Collectibles, Started First Level

---
+++ Assets/Scripts/Float.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Float : MonoBehaviour
+{
+    public float floatSpeed = 1.0f; // Adjust this to control the float speed.
+    public float floatHeight = 0.5f; // Adjust this to control the float height.
+
+    private Vector3 initialPosition;
+
+    private void Start()
+    {
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        // Calculate the new Y position based on a sine wave.
+        float newY = initialPosition.y + Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+
+        // Update the object's position with the new Y value.
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     private bool isDashing;
     private float dashingPower = 18f;
     private float dashingTime = 0.2f;
-    private float dashingCooldown = 1f;
+    private float dashingCooldown = 0.5f;
 
     //Wall Sliding & Jumping
     private bool isWallSliding;
@@ -121,11 +121,14 @@ public class PlayerMovement : MonoBehaviour
     {
         if (IsWalled() && !IsGrounded() && horizontal != 0f)
         {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsWalled", true);
             isWallSliding = true;
             rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
         }
         else
         {
+            animator.SetBool("IsWalled", false);
             isWallSliding = false;
         }
     }
