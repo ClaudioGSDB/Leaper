472773a65478688bba12653696eabb135998db27
Claudio Sciotto <fisizion@gmail.com>
2023-10-11 20:20:00 -0400

    started Level creating, added an enemy

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public Transform patrolCenter; // Drag the center of the block here
+    public float patrolSpeed = 2.0f;
+    public float patrolRadius = 5.0f;
+    public float cornerPauseTime = 2.0f;
+
+    private float currentAngle = 0.0f;
+    private float currentPauseTime = 0.0f;
+
+    void Update()
+    {
+        if (currentPauseTime > 0)
+        {
+            currentPauseTime -= Time.deltaTime;
+            return;
+        }
+
+        currentAngle += patrolSpeed * Time.deltaTime;
+
+        if (currentAngle >= 360.0f)
+        {
+            currentAngle = 0.0f;
+            currentPauseTime = cornerPauseTime;
+        }
+
+        float angleInRadians = currentAngle * Mathf.Deg2Rad;
+        float x = patrolCenter.position.x + Mathf.Cos(angleInRadians) * patrolRadius;
+        float y = patrolCenter.position.y + Mathf.Sin(angleInRadians) * patrolRadius;
+
+        transform.position = new Vector2(x, y);
+    }
+}
